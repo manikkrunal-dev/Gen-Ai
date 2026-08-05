1c631a86a65a1ba8aefa4697a98ed1636bb290d6
manikkrunal-dev <manikkrunal.dev@gmail.com>
2026-08-05 23:05:56 +0530

    Create Frontend, Add API

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useNavigate,Link } from 'react-router'
+
+const Register = () => {
+
+  const navigate = useNavigate()
+  const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+    
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Register</h1>
+
+            <form>
+
+            <div className="input-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" id='username' name='username' placeholder='Enter username'/>
+            </div>
+
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id='email' name='email' placeholder='Enter email address'/>
+            </div>
+
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" placeholder='Enter Password' />
+            </div>
+                
+            <button className='button primary-button'> Register </button>
+
+            </form>
+
+            <p>Already have an account? <Link to={"/login"}>Login</Link> </p>
+        </div>
+    </main>
+  )
+}
+
+export default Register
